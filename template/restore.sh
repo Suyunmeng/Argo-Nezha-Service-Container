bfd2079dfecc32a31fe254b97dc603d6eb4c3f41
Suyunmeng <sumengjing@outlook.com>
2024-12-13 13:37:46 +0800

    update

--- template/restore.sh
+++ template/restore.sh
@@ -149,7 +149,7 @@ if [ -e $TEMP_DIR/backup.tar.gz ]; then
     [ $(type -p sqlite3) ] || apt-get -y install sqlite3
     DB_TOKEN=$(awk -F ': ' '/^agentsecretkey:/ {print $2}' ${TEMP_DIR}/${FILE_PATH}data/config.yaml)
     [ -n "$DB_TOKEN" ] && LOCAL_TOKEN=$(awk -F ': ' '/^client_secret:/ {print substr($2, 2, length($2)-2)}' /dashboard/agent/agent.yml)
-    [ "$DB_TOKEN" != "$LOCAL_TOKEN" ] && UID=$(awk -F ': ' '/^uuid:/ {print substr($2, 2, length($2)-2)}' /dashboard/agent/agent.yml) && sqlite3 ${TEMP_DIR}/${FILE_PATH}data/sqlite.db "update servers set uuid='${UID}' where created_at='2024-12-11 15:19:41.6189758+08:00'" && sed -i "s/^client_secret: .*/client_secret: ${DB_TOKEN}/" "/dashboard/agent/agent.yml"
+    [ "$DB_TOKEN" != "$LOCAL_TOKEN" ] && ID=$(awk -F ': ' '/^uuid:/ {print substr($2, 2, length($2)-2)}' /dashboard/agent/agent.yml) && sqlite3 ${TEMP_DIR}/${FILE_PATH}data/sqlite.db "update servers set uuid='${ID}' where created_at='2024-12-11 15:19:41.6189758+08:00'" && sed -i "s/^client_secret: .*/client_secret: ${DB_TOKEN}/" "/dashboard/agent/agent.yml"
   fi
 
   # 复制临时文件到正式的工作文件夹
