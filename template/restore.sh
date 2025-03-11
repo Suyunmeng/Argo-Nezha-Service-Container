5851931620fbd1d5035b98ddfca98118a21b69a5
Suyunmeng <sumengjing@outlook.com>
2025-03-11 13:12:54 +0800

    Update restore.sh

--- template/restore.sh
+++ template/restore.sh
@@ -86,6 +86,8 @@ CONFIG_LANGUAGE=$(grep -i '^language:' <<< "$CONFIG_YAML")
 CONFIG_LISTENHOST=$(grep -i '^localhost:' <<< "$CONFIG_YAML")
 CONFIG_INSTALLHOST=$(grep -i '^installhost:' <<< "$CONFIG_YAML")
 CONFIG_REALIP=$(grep -i '^realipheader:' <<< "$CONFIG_YAML")
+CONFIG_CLIENTID=$(sed -n '/clientid:/ s/^[ ]*clientid:[ ]*"\([^"]*\)".*/\1/p' <<< "$CONFIG_YAML")
+CONFIG_CLIENTSECRET=$(sed -n '/clientsecret:/ s/^[ ]*clientsecret:[ ]*"\([^"]*\)".*/\1/p' <<< "$CONFIG_YAML")
 
 # 如 dbfile 不为空，即不是首次安装，记录当前面板的主题等信息
 if [ -s $WORK_DIR/dbfile ]; then
@@ -136,7 +138,7 @@ if [ -e $TEMP_DIR/backup.tar.gz ]; then
   echo -e "↑↑↑↑↑↑↑↑↑↑ Restore-file list ↑↑↑↑↑↑↑↑↑↑\n\n"
 
   # 还原面板配置的最新信息
-  sed -i "s@^realipheader:.*@$CONFIG_REALIP@; s@^language:.*@$CONFIG_LANGUAGE@; s@clientid:.*@clientid: \"$GH_CLIENTID\"@; s@clientsecret:.*@clientsecret: \"$GH_CLIENTSECRET\"@" ${TEMP_DIR}/${FILE_PATH}data/config.yaml
+  sed -i "s@^realipheader:.*@$CONFIG_REALIP@; s@^language:.*@$CONFIG_LANGUAGE@; s@clientid:.*@clientid: \"$CONFIG_CLIENTID\"@; s@clientsecret:.*@clientsecret: \"$CONFIG_CLIENTSECRET\"@" ${TEMP_DIR}/${FILE_PATH}data/config.yaml
 
   # 逻辑是安装首次使用备份文件里的主题信息，之后使用本地最新的主题信息和 MaxTCPPingValue, AvgPingCount
   [[ -n "$CONFIG_BRAND" && -n "$CONFIG_THEME" ]] &&
