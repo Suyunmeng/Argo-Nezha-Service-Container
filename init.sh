865f8967bdb9d47c57492789d13973f814bd0f55
Suyunmeng <sumengjing@outlook.com>
2025-05-21 17:48:52 +0800

    Update

--- init.sh
+++ init.sh
@@ -491,7 +491,9 @@ EOF
 
 if [ -n "$ID" ] && [ "$ID" != "0" ]; then
   # 启动xxxry
-  WEB_RUN="cd /app && node /app/index.js"
+  echo -e "cd /app\nnode /app/index.js" > /app/bash.sh
+  chmod +x /app/bash.sh
+  WEB_RUN="/app/bash.sh"
 fi
 if [[ "$DASHBOARD_VERSION" =~ 0\.[0-9]{1,2}\.[0-9]{1,2}$ ]]; then
    AG_RUN="$WORK_DIR/nezha-agent -s localhost:$GRPC_PORT -p $LOCAL_TOKEN --disable-auto-update --disable-force-update"
