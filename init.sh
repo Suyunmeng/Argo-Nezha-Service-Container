d796207de5f861d16bbd34afbfa792898375f053
Suyunmeng <Susus0175@proton.me>
2025-11-15 10:13:50 +0800

    feat(oidc): Add Cloudflare Access Types

--- init.sh
+++ init.sh
@@ -340,6 +340,20 @@ oauth2:
      user_info_url: "https://api.github.com/user"
      user_id_path: "id"
 EOF
+    if [[ -n "$CF_CLIENTID" && -n "$CF_CLIENTSECRET" && -n "$CF_AUTHURL" && -n "$CF_TOKENURL" && -n "$CF_TOKENINFOURL" ]]; then
+      cat >> ${WORK_DIR}/data/config.yaml << EOF
+   Cloudflare:
+     client_id: "$CF_CLIENTID"
+     client_secret: "$CF_CLIENTSECRET"
+     endpoint:
+       auth_url: "$CF_AUTHURL"
+       token_url: "$CF_TOKENURL"
+     scopes:
+       - openid
+       - profile
+     user_info_url: "$CF_TOKENINFOURL"
+     user_id_path: "sub"
+EOF
     fi
     mkdir ${WORK_DIR}/agent
     cat > ${WORK_DIR}/agent/config.yml << EOF
